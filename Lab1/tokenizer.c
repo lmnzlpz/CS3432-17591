48be801d9ccaf0ad70599307b864b4f25450ea10
Leonardo Munoz <lmnz.lpz.gmail.com>
2021-09-14 11:30:48 -0600

    I created the tokenizer.c file based on the skeleton. I also added a main function to it to ensure it compiles. At the moment, only the first two functions have been implemented.

---
+++ Lab1/tokenizer.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/* Return true (non-zero) if c is a whitespace character
+   ('\t', ' ').
+   Zero terminators are not printable (therefor false).
+ */
+bool delim_character (char c){
+  if (c == '\t' || c ==' ')
+    return true;
+  else
+    return false;
+}
+
+/* Return true (non-zero) if c is a non-whitespace character
+   (not tab or space). Zero terminators are not printable
+   (therefore false).
+*/
+bool non_delim_character(char c ){
+  if (c == '\t' || c == ' ' || c == '\0')
+    return false;
+  else
+    return true;
+}
+
+/* Returns a pointer to the first character of the next
+   space-separated word.
+*/
+char *word_start(char* str){
+}
+
+/* Returns a pointer to the first space character of the zero
+   terminated string.
+*/
+char *end_word(char* str){}
+
+/* counts the number of words or tokens*/
+int count_tokens(char* str){}
+
+/* Returns a freshly allocated zero-terminated vector of freshly 
+   allocated space-seperated tokens from zero-terminated str.
+   For example, tokenize("hello world string") would result in:
+     tokens[0] = "hello"
+     tokens[1] = "world"
+     tokens[2] = "string"
+     tokens[3] = 0
+*/
+char *copy_str(char *inStr, short len){}
+
+char** tokenize(char* str){}
+
+void print_all_tokens(char** tokens){}
+
+int main(){
+  tokenize("Hello there");
+}
+
+
+  
