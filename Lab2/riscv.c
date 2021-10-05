42ed2593e6cb754abe141dbaae38f3e13ea5fb76
Leonardo Munoz <lmnz.lpz.gmail.com>
2021-10-05 12:13:39 -0600

    Added the functionality to go into the proper state depending on first INPUT.

--- Lab2/riscv.c
+++ Lab2/riscv.c
@@ -10,6 +10,44 @@ int32_t* reg; // Array of 32 32-bit registers
 void init_regs();
 bool interpret(char* instr);
 void write_read_demo();
+bool areSame(char* str1, char* str2);
+int initialCaseHandler(char* str);
+
+int initialCaseHandler(char* str){
+  if (areSame(str, "LW")) {return 0;}
+  else if (areSame(str, "SW")) {return 1;}
+  else if (areSame(str, "ADD")) {return 2;}
+  else if (areSame(str, "ADDI")) {return 3;}
+  else if (areSame(str, "AND")) {return 4;}
+  else if (areSame(str, "OR")) {return 5;}
+  else if (areSame(str, "XOR")) {return 6;}
+  else
+    return -1;
+}
+
+bool areSame(char* str1, char* str2){
+  char* temp1 = str1;
+  char* temp2 = str2;
+  int count1 = 0;
+  int count2 = 0;
+
+  while (*(temp1+count1) != '\0'){
+    count1++;
+  }
+  while (*(temp2+count2) != '\0'){
+    count2++;
+  }
+  printf("%d || %d\n", count1, count2);
+  if (count1-1 != count2){return false;}
+  
+  int i = 0;
+  while (*(str1+i) != '\0' && *(str2+i) != '\0'){
+    if (*(str1+i) != *(str2+i))
+      return false;
+    i++;
+  }
+  return true;
+}
 
 /**
  * Initialize register array for usage.
@@ -31,9 +69,38 @@ void init_regs(){
  * as a parameter to this function.
  */
 bool interpret(char* instr){
+  char **pointerStorage;
+  pointerStorage = tokenize(instr, ' ');
+  print_all_tokens(pointerStorage);
+  int caseNum = initialCaseHandler(*pointerStorage);
+  switch(caseNum){
+  case 0:
+    printf("LW read");
+    break;
+  case 1:
+    printf("SW read");
+    break;
+  case 2:
+    printf("ADD read");
+    break;
+  case 3:
+    printf("ADDI read");
+    break;
+  case 4:
+    printf("AND read");
+    break;
+  case 5:
+    printf("OR read");
+    break;
+  case 6:
+    printf("XOR read");
+    break;
+  default:
+    printf("ERROR");
+    return false;
+  }
   return true;
 }
-
 
 /**
  * Simple demo program to show the usage of read_address() and write_address() found in memory.c
@@ -63,23 +130,17 @@ int main(){
 	// Do not write any code between init_regs
 	init_regs(); // DO NOT REMOVE THIS LINE
 
-	char input[100]; // User input can only be 100 chars long.
-	char **pointerStorage;
-	printf("Press 'x' to quit.\n");
+	char input[1000]; // User input can only be 100 chars long.
+	printf("Enter 'x' to quit.\n");
 	while(1){
 	  printf("Input: ");
 	  fgets(input, sizeof(input), stdin);
 	  if (*input=='x'){break;}
-	  int count = count_tokens(input, ' ');
-	  pointerStorage = tokenize(input, ' ');
-	  print_all_tokens(pointerStorage);
-	  for (int i = 0; i < count; i++){
-	    interpret(pointerStorage+i);
-	  }
+	  interpret(input);
 	  printf("\n");
 	}
 	// Below is a sample program to a write-read. Overwrite this with your own code.
-	//write_read_demo();
+	write_read_demo();
 
 	return 0;
 }
